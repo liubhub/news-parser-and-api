78505e9a5b3a1236ceb0378ea995fb940cf3ebc1
liubhub <liuhub.dev@gmail.com>
2018-08-16 16:01:14 +0300

    now using socket io instead of event stream

--- parse-and-save/app.py
+++ parse-and-save/app.py
@@ -1,7 +1,7 @@
 import json
 import time
-from flask import Flask, Response
-from flask import render_template
+from flask import Flask, Response,render_template
+from flask_socketio import SocketIO, emit
 
 import pymongo
 
@@ -10,52 +10,32 @@ from updates import check_and_filter_updates, parse_and_add_updates_to_db
 
 app = Flask(__name__)
 
+app.debug = True
+
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client.news
 collection = db.news
 
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def main():
-    # need to render template
-    # return str(collection.find_one())
     return render_template('index.html')
- 
-
-
-@app.route('/update')
-def progress():
-    print('Generating!!!')
-    def generate():
-        x = 0
-
-        while x <= 100:
-            print("data:" + str(x) + "\n\n")
-            yield "data:" + str(x) + "\n\n"
-            x = x + 10
-            time.sleep(0.5)
-
-    return Response(generate(), mimetype= 'text/event-stream')
-
 
 # @app.route('/update')
-# def update():
-#     # Get request on updating database
+@socketio.on('update')
+def progress():
 
-#     articles = check_and_filter_updates(collection)
-#     if articles:
-#         res = parse_and_add_updates_to_db(articles, collection)
-#         if res:
-#             return 'Updated'
-#         else:
-#             'Something went wrong'
-#     else:
-#         return 'Up to date'
     
-#     return 'Hi'
 
 
+    emit('update_response',
+        {'data': 'Hello'}
+    )
+    
 
+    
 
 @app.route('/add')
 def add():
@@ -70,4 +50,5 @@ def fetch():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    socketio.run(app)
--- parse-and-save/static/script.js
+++ parse-and-save/static/script.js
@@ -6,22 +6,31 @@ $(document).ready(function () {
     $('#check-updates').click(function () {
 
         console.log('Clicked')
+
+
         $('.progress').fadeIn();
         
+        var socket = io.connect('http://127.0.0.1:5000/');
 
-        var source = new EventSource("/update");
 
-        source.onmessage = function (event) {
+        socket.emit('update');
 
-            console.log(event.data)
+        socket.on('update_response', function(data){
+            console.log(data)
+        })
+        // var source = new EventSource("/update");
 
-            $('.progress-bar').css('width', event.data + '%').attr('aria-valuenow', event.data);
-            $('.progress-bar-label').text(event.data + '%');
+        // source.onmessage = function (event) {
 
-            if (event.data == 100) {
-                source.close()
-            }
-        }
+        //     console.log(event.data)
+
+        //     $('.progress-bar').css('width', event.data + '%').attr('aria-valuenow', event.data);
+        //     $('.progress-bar-label').text(event.data + '%');
+
+        //     if (event.data == 100) {
+        //         source.close()
+        //     }
+        // }
 
     })
 
